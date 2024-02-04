21aedf3e903b3f3aa120acd6d1501d74297f5fce
averyevy <106488938+averyevy@users.noreply.github.com>
2024-02-03 22:02:04 -0800

    Create 2022 Blocks.cpp

---
+++ src/cpp/2022 Blocks.cpp
@@ -0,0 +1,70 @@
+//http://usaco.org/index.php?page=viewproblem2&cpid=1205
+
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+  
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+ 
+const int MOD = 1000000007;
+double PI = 4*atan(1);
+ 
+#define IO(NAME) \
+    cin.tie(0)->sync_with_stdio(0); \
+    if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+    freopen(NAME ".out","w",stdout); 
+
+
+template<template <typename> class P = std::less > 
+struct compare_pair_second { 
+    template<class T1, class T2> bool operator()(const std::pair<T1, T2>& left, const std::pair<T1, T2>& right) {
+        return P<T2>()(left.second, right.second);
+    }
+};
+  
+#define log(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); \
+stringstream _ss(_s); istream_iterator<string> _it(_ss);  cout<< "LOG - line:" << __LINE__<< "      "; err(_it, args); cout <<endl;}
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a <<"; ";
+	err(++it, args...);
+}
+
+bool solve(array<string, 4> blocks) {
+    string word;
+    cin >> word;
+   
+    do {
+        bool ok = true;
+        for(int i=0;i<word.size();i++) {
+            if (find(blocks[i].begin(), blocks[i].end(), word[i]) == blocks[i].end()) ok = false;
+        }
+        if (ok) return true;
+    } while (next_permutation(blocks.begin(), blocks.end()));
+	return false;
+}
+
+int main() {
+    //IO("hps");
+    int N;
+    cin>>N;
+    array<string,4> blocks;
+    for(int i=0; i<4; i++) cin>>blocks[i];
+    sort(blocks.begin(), blocks.end());
+    for(int i=0; i <N; i ++ ) {
+    bool b = solve(blocks);
+    if (b) cout << "YES" << endl;
+    else cout << "NO" <<endl;
+    }
+    return 0;
+}
